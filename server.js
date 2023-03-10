9ed95d1bb2b4e6cdbcef5f6d87a4b7f8e1ecd8f6
sakthivel213 <109096713+sakthivel213@users.noreply.github.com>
2023-03-10 17:04:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app= express();
 app.use(cors())
 app.use(express.json())
 const mongoose= require('mongoose')
-mongoose.connect('mongodb+srv://sakthivel:tirunelveli@cluster0.xth57f5.mongodb.net/test',{ useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('connected')).catch((err)=>{
+mongoose.connect('mongodb+srv://sakthivel:tirunelveli@cluster0.xth57f5.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('connected')).catch((err)=>{
     console.log(err)
 })
 dotenv.config()
