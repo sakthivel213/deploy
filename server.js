6a3c8b31042a8406f0b95497ee2c564ed6351577
sakthivel213 <109096713+sakthivel213@users.noreply.github.com>
2023-03-10 15:39:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app= express();
 app.use(cors())
 app.use(express.json())
 const mongoose= require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/guest').then(console.log('connected')).catch((err)=>{
+mongoose.connect('mongodb://127.0.0.1:27017/guest', { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('connected')).catch((err)=>{
     console.log(err)
 })
 dotenv.config()
